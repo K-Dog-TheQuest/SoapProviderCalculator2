50d7e4a157a84c7540ef29065e1b6b41ecfdffe7
Korhan <Korhan@LAPTOP-476E8TDB>
2017-09-26 10:14:15 +0200

    Add project files.

---
+++ SoapProviderCalculatorTests/Service1Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoapProviderCalculator;
+using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapProviderCalculator.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            
+        }
+        [TestMethod()]
+        public void SubTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
